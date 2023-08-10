cee935c1fbd5c1aa06684142eee12581b13e5e51
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-08-10 12:00:36 -0700

    by

--- models/user.py
+++ models/user.py
@@ -2,4 +2,9 @@
 from models.base_model import BaseModel
 
 class User(BaseModel):
+    """rrrrrrrrrrrrrrrrrrrrrrr"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
     pass
